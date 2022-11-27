1fe920e8635648963852785f41bce1a818f1a139
jaedeokhan <hjaedeok@gmail.com>
2022-11-27 16:44:42 +0900

    event - React 사용자 정의 태그에서 함수를 선언    - <Nav topcis={topics} onChangeMode={(id) => {alert(id);}}></Nav> - React 선언 한 함수를 구현부에서 사용    - props.onChangeMode();

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function Header(props) {
   return (
     <header>
-        <h1><a href="/">{props.title}</a></h1>
+        <h1><a href="/" onClick={(event) => {
+          event.preventDefault();
+          props.onChangeMode();
+        }}>{props.title}</a></h1>
     </header>
   )  
 }
@@ -13,7 +16,13 @@ function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topcis.length; i++){
     let t = props.topcis[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+    lis.push(
+    <li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>);
   }
   return (
     <nav>
@@ -41,8 +50,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav topcis={topics}></Nav>
+      <Header title="REACT" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topcis={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
