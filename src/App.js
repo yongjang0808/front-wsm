b659e1c981ecd7fa57884146d3de87e4d4e87674
jaedeokhan <hjaedeok@gmail.com>
2022-11-27 23:24:05 +0900

    Delete - <></> 태그로 변수에 여러 태그 및 값 할당

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
+const MODE_WELCOME = 'WELCOME';
+const MODE_READ = 'READ';
+const MODE_CREATE = 'CREATE';
+const MODE_UPDATE = 'UPDATE';
+
 function Header(props) {
   return (
     <header>
@@ -92,7 +97,7 @@ function Update(props) {
 }
 
 function App() {
-  const [mode, setMode] = useState('WELCOME');
+  const [mode, setMode] = useState(MODE_WELCOME);
   const [id, setId] = useState(null);
   const [nextId, setNextId] = useState(4);
   const [topics, setTopics] = useState([
@@ -104,9 +109,9 @@ function App() {
   let content = null;
   let contextControl = null;
 
-  if (mode === 'WELCOME') {
+  if (mode === MODE_WELCOME) {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
-  } else if (mode === 'READ') {
+  } else if (mode === MODE_READ) {
     let title, body = null;
     for (let i = 0; i < topics.length; i++){
       if (topics[i].id === id){
@@ -115,21 +120,34 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={'/update/' + id} onClick={(event) => {
-      event.preventDefault();
-      setMode('UPDATE');
-    }}>Update</a></li>
-  } else if (mode === 'CREATE') {
+    contextControl = <>
+      <li><a href={'/update/' + id} onClick={(event) => {
+            event.preventDefault();
+            setMode(MODE_UPDATE);
+            }}>Update</a>         
+      </li>
+      <li><input type="button" value="Delete" onClick={() => {
+        const newTopics = [];
+        for (let i = 0; i < topics.length; i++){
+          if (topics[i].id !== id){
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode(MODE_WELCOME);
+      }}/></li>
+    </>  
+  } else if (mode === MODE_CREATE) {
     content = <Create onCreate={(_title, _body) => {
       const newTopic = {id:nextId, title:_title, body:_body};
       const newTopics = [...topics];
       newTopics.push(newTopic);
       setTopics(newTopics)
-      setMode('READ');
+      setMode(MODE_READ);
       setId(nextId);
       setNextId(nextId + 1);
     }}></Create>
-  } else if (mode === 'UPDATE') {
+  } else if (mode === MODE_UPDATE) {
     let title, body = null;
     for (let i = 0; i < topics.length; i++){
       if (topics[i].id === id){
@@ -147,18 +165,18 @@ function App() {
         }
       }
       setTopics(newTopics);
-      setMode('READ');
+      setMode(MODE_READ);
     }}></Update>
   }
 
   return (
     <div>
       <Header title="WEB" onChangeMode={() => {
-        setMode('WELCOME');
+        setMode(MODE_WELCOME);
       }}></Header>
 
       <Nav topcis={topics} onChangeMode={(_id) => {
-        setMode('READ');
+        setMode(MODE_READ);
         setId(_id);
       }}></Nav>
 
@@ -167,7 +185,7 @@ function App() {
         <li>
             <a href="/create" onClick={(event) => {
             event.preventDefault();
-            setMode('CREATE');
+            setMode(MODE_CREATE);
             }}>Create</a>
         </li>
         {contextControl}
