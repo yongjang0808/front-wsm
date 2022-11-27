eba7bcdbbc20a92a842905da197df540b6323828
jaedeokhan <hjaedeok@gmail.com>
2022-11-27 18:59:06 +0900

    Create - 값 복사    - const newTopics = [...topics];

--- src/App.js
+++ src/App.js
@@ -43,14 +43,33 @@ function Article(props) {
   )
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder="title"/></p>
+        <p><textarea name="body" placeholder="body"></textarea></p>
+        <p><input type="submit" value="Create"/></p>
+      </form>
+    </article>
+  )
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id : 1, title : 'html', body : 'html is ...'},
     {id : 2, title : 'css', body : 'css is ...'},
     {id : 3, title : 'js', body : 'js is ...'}
-  ];
+  ]);
   let content = null;
 
   if (mode === 'WELCOME') {
@@ -64,6 +83,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  } else if (mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = {id:nextId, title:_title, body:_body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics)
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
   }
 
   return (
@@ -71,11 +100,18 @@ function App() {
       <Header title="WEB" onChangeMode={() => {
         setMode('WELCOME');
       }}></Header>
+
       <Nav topcis={topics} onChangeMode={(_id) => {
         setMode('READ');
         setId(_id);
       }}></Nav>
+
       {content}
+
+      <a href="/create" onClick={(event) => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
